78f90a420e86ead99f09c5fd2ed65746f9e35fff
Reihaneh75 <azarianreihaneh@gmail.com>
2019-09-21 12:23:32 +0430

    style login and slider

--- dist/Angular7/assets/img/main.js
+++
@@ -1,6 +0,0 @@
-// This is "probably" IE9 compatible but will need some fallbacks for IE8
-// - (event listeners, forEach loop)
-
-// wait for the entire page to finish loading
-
-
--- dist/Angular7/polyfills.js
+++ dist/Angular7/polyfills.js
@@ -5783,8 +5783,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Seemorgh-132\Desktop\ma\Angular\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! C:\Users\Seemorgh-132\Desktop\ma\Angular\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! C:\Users\Seemorgh-140\Documents\Angular Projects\Music-Atlas-UI\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\Seemorgh-140\Documents\Angular Projects\Music-Atlas-UI\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
